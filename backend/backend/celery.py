056a93b9cba91a867c2d898ea8f4d4529f86c87d
Ankushsb62 <ankushgupta200328@gmail.com>
2025-04-22 12:06:04 +0530

    ok

---
+++ backend/backend/celery.py
@@ -0,0 +1,24 @@
+import os
+from time import sleep
+from celery import Celery
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
+app = Celery('backend')
+app.conf.broker_url = os.environ.get('REDIS_URL', 'redis://redis:6379/0')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+
+@app.task
+def add(x,y):
+    sleep(20)
+    return x + y
+
+
+#shedule code
+# app.conf.beat_schedule = {
+#     'every-10-seconds': {
+#         'task': 'myapp.tasks.clear_session_cache',
+#         'schedule': 10.0,
+#         'args': ("14545",)
+#     }
+# }
